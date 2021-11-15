63e8db354340ba5fd3a3249de1b3eb953cbf429a
Zared316 <zared316@gmail.com>
2021-11-15 05:19:13 -0500

    Challenge branch

---
+++ dig3480-f21-t3-rubys_new_adventure-Zared316/Assets/Scripts/DamageZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    public AudioClip Damaged;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController >();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+            controller.PlaySound(Damaged);
+        }
+    }
+}
